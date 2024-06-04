57b2cad288b15f446192e53257b7ca37d9613a35
Harshit Tomar <harshitt345@gmail.com>
2024-06-04 09:54:27 +0530

    Responding with a bare-bones html page

--- Assignment 2/server/src/main.rs
+++ Assignment 2/server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
@@ -20,12 +21,16 @@ fn handle_connection(mut stream: TcpStream) {
         .map(|result| result.unwrap()) // TODO: Handling them gracefully maybe?
         .take_while(|line| !line.is_empty()) // Reading till an empty line cuz spec
         .collect();
-    //
+
     // println!("Request: {:#?}", http_request);
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let length = contents.len();
 
-    stream.write_all(response.as_bytes()).unwrap();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap(); // TODO: Handling gracefully
 
     let req_line_split: Vec<_> = http_request[0].trim().split_whitespace().collect();
     println!("Responded to request to {}", req_line_split[1]);
