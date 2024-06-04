14e4ef94064c97ae4b30fd44e9a0df350c15406a
Harshit Tomar <harshitt345@gmail.com>
2024-06-04 08:06:50 +0530

    Only printing the good stuff

--- Assignment 2/server/src/main.rs
+++ Assignment 2/server/src/main.rs
@@ -14,16 +14,19 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    // let buf_reader = BufReader::new(&mut stream);
-    // let http_request: Vec<_> = buf_reader
-    //     .lines() // getting all the lines in the request
-    //     .map(|result| result.unwrap()) // TODO: Handling them gracefully maybe?
-    //     .take_while(|line| !line.is_empty()) // Reading till an empty line cuz spec
-    //     .collect();
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines() // getting all the lines in the request
+        .map(|result| result.unwrap()) // TODO: Handling them gracefully maybe?
+        .take_while(|line| !line.is_empty()) // Reading till an empty line cuz spec
+        .collect();
     //
     // println!("Request: {:#?}", http_request);
 
     let response = "HTTP/1.1 200 OK\r\n\r\n";
 
     stream.write_all(response.as_bytes()).unwrap();
+
+    let req_line_split: Vec<_> = http_request[0].trim().split_whitespace().collect();
+    println!("Responded to request to {}", req_line_split[1]);
 }
