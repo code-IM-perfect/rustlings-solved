cf0c3dda8701ab0453b94c0284a7490425d633d3
Harshit Tomar <harshitt345@gmail.com>
2024-06-04 07:05:21 +0530

    giving out response code 200 to everything

--- Assignment 2/server/src/main.rs
+++ Assignment 2/server/src/main.rs
@@ -14,12 +14,16 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines() // getting all the lines in the request
-        .map(|result| result.unwrap()) // TODO: Handling them gracefully maybe?
-        .take_while(|line| !line.is_empty()) // Reading till an empty line cuz spec
-        .collect();
+    // let buf_reader = BufReader::new(&mut stream);
+    // let http_request: Vec<_> = buf_reader
+    //     .lines() // getting all the lines in the request
+    //     .map(|result| result.unwrap()) // TODO: Handling them gracefully maybe?
+    //     .take_while(|line| !line.is_empty()) // Reading till an empty line cuz spec
+    //     .collect();
+    //
+    // println!("Request: {:#?}", http_request);
 
-    println!("Request: {:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
