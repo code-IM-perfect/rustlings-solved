f5cb4a65a0adbe8c341157e5f16138604dd862b5
Harshit Tomar <harshitt345@gmail.com>
2024-06-05 09:20:19 +0530

    An actual real server

--- Assignment 2/server/src/main.rs
+++ Assignment 2/server/src/main.rs
@@ -2,7 +2,28 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    path::Path,
 };
+
+struct Response {
+    status_line: String,
+    contents: String,
+}
+
+impl Response {
+    fn length(&self) -> usize {
+        self.contents.len()
+    }
+
+    fn string(&self) -> String {
+        format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            self.status_line,
+            self.length(),
+            self.contents
+        )
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7567").unwrap();
@@ -16,22 +37,69 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines() // getting all the lines in the request
-        .map(|result| result.unwrap()) // TODO: Handling them gracefully maybe?
-        .take_while(|line| !line.is_empty()) // Reading till an empty line cuz spec
-        .collect();
+    let request_line = match buf_reader.lines().next().unwrap() {
+        Ok(line) => line, // Req line of the form: "GET / HTTP/1.1"
+        Err(_) => {
+            println!("There was an error reading the request");
+            String::from("bruh")
+        }
+    };
+    let requested_path = request_line.trim().split_whitespace().nth(1);
 
-    // println!("Request: {:#?}", http_request);
+    let response = match requested_path {
+        Some(req_path) => {
+            let mut req_path = "root".to_string() + req_path;
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
-    let length = contents.len();
+            if req_path.chars().last().unwrap() == '/' {
+                let test_path = req_path.clone() + "index.html";
+                match Path::new(&test_path).try_exists() {
+                    Ok(true) => {
+                        req_path = test_path;
+                    }
+                    _ => {}
+                }
+            }
+            match fs::read_to_string(&req_path) {
+                Ok(file_contents) => Response {
+                    status_line: "HTTP/1.1 200 OK".to_string(),
+                    contents: file_contents,
+                },
+                Err(_) => match Path::new(&req_path).try_exists() {
+                    Ok(true) => Response {
+                        status_line: "HTTP/1.1 403 Forbidden".to_string(),
+                        contents: fs::read_to_string("root/403.html").unwrap(),
+                    },
+                    Ok(false) => Response {
+                        status_line: "HTTP/1.1 404 Not Found".to_string(),
+                        contents: fs::read_to_string("root/404.html").unwrap(),
+                    },
+                    Err(_) => Response {
+                        status_line: "HTTP/1.1 500 Internal Server Error".to_string(),
+                        contents: fs::read_to_string("root/500.html").unwrap(),
+                    },
+                },
+            }
+        }
+        None => Response {
+            status_line: "HTTP/1.1 400 Bad Request".to_string(),
+            contents: String::new(),
+        },
+    };
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap(); // TODO: Handling gracefully
-
-    let req_line_split: Vec<_> = http_request[0].trim().split_whitespace().collect();
-    println!("Responded to request to {}", req_line_split[1]);
+    match stream.write_all(response.string().as_bytes()) {
+        Ok(_) => match requested_path {
+            Some(req_path) => {
+                println!("Responded to request to {}", req_path);
+            }
+            None => {
+                println!("Request was malformed, responded with a 400: Bad Request");
+            }
+        },
+        Err(_) => {
+            println!(
+                "Could not respond to request to {}",
+                requested_path.unwrap()
+            );
+        }
+    };
 }
